2ee70c8796a0bc6b8cdc600c6d81182db24a5fa1
keerthi15 <keerthi0898@gmail.com>
2023-09-19 00:28:03 +0530

    Update comments, and npm scripts

--- src/services/message_labels.service.js
+++ src/services/message_labels.service.js
@@ -4,11 +4,23 @@ const { getArrayMap } = require('../handlers/utils');
 const sequelize = require('../models/db')(gmailDb);
 const { messages_label_mapping: MessageLabels } = require('../models/init-models')(sequelize);
 
+/**
+ * Delete existing labels of a message
+ * Create new relevant labels of the message
+ *
+ * @param {Number} messageId
+ * @param {[Number]} labelIds
+ */
 const upsertMessageLabels = async (messageId, labelIds) => {
     await deleteMessageLabelMappings(messageId);
     await createMessageLabelMapping(labelIds);
 };
 
+/**
+ * Returns messages mapped to a label
+ * @param {[Number]} labelIds
+ * @return {Object}
+ */
 const getMessageLabels = async (labelIds) => {
     const messageLabels = await MessageLabels.findAll({
         where: {
@@ -25,10 +37,18 @@ const getMessageLabels = async (labelIds) => {
     return messageLabelMap;
 };
 
+/**
+ * Creates multiple message label records
+ * @param {[Object]} labelIds
+ */
 const createMessageLabelMapping = async (labelIds) => {
     await MessageLabels.bulkCreate(labelIds);
 };
 
+/**
+ * Deletes labels mapping of a specific message id
+ * @param {Number} messageId
+ */
 const deleteMessageLabelMappings = async (messageId) => {
     await MessageLabels.destroy({
         where: {
@@ -37,6 +57,11 @@ const deleteMessageLabelMappings = async (messageId) => {
     });
 };
 
+/**
+ * Returns all labels mapped to a message
+ * @param {[Number]} messageIds
+ * @return {[Number]}
+ */
 const getLabelsByMessageId = async (messageIds) => {
     const messageLabels = await MessageLabels.findAll({
         attr: [ 'label_id' ],
@@ -50,7 +75,13 @@ const getLabelsByMessageId = async (messageIds) => {
     return labelIds;
 };
 
-
+/**
+ * Create message - label mapping objects for a particular message
+ * @param {Number} messageId
+ * @param {[String]} messageLabels
+ * @param {Object} labelsMap
+ * @return {Object}
+ */
 const constructMessageLabelMapping = (messageId, messageLabels, labelsMap) => {
     const messageLabelMap = [];
     messageLabels.forEach((label) => {
